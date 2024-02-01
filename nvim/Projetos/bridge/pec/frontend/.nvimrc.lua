4dc9e253e4f082458638cd20765a8a308b8312a2
Pedro Binotto <pedrosantibinotto@gmail.com>
2024-01-31 23:56:52 -0300

    31-01-24

--- nvim/Projetos/bridge/pec/frontend/.nvimrc.lua
+++ nvim/Projetos/bridge/pec/frontend/.nvimrc.lua
@@ -2,4 +2,4 @@ vim.g.RunBackend =
 	'cd $(git rev-parse --show-toplevel)$(echo "/app-bundle") && mvn spring-boot:run -Dspring.profiles.active=dev,dev-postgres -Dbridge.flags.experimental=true'
 vim.g.RunFrontend = 'cd $(git rev-parse --show-toplevel)$(echo "/frontend") && HTTPS=true BROWSER=none yarn start'
 vim.g.RunTests = 'cd $(git rev-parse --show-toplevel)$(echo "/frontend") && yarn test'
-vim.g.BuildApp = "$(git rev-parse --show-toplevel) && mvn clean install -DskipTests"
+vim.g.BuildApp = "cd $(git rev-parse --show-toplevel) && mvn clean install -DskipTests"
