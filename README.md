9c2fd3e13b1b0861615e1801df5f8f0509c53909
Pedro Binotto <pedrosantibinotto@gmail.com>
2024-09-26 18:05:49 -0300

    update sep26-24

---
+++ README.md
@@ -0,0 +1,23 @@
+# .dofiles
+
+Pre-requisites:
+- `git`;
+- `make`;
+- `stow`;
+
+On Ubuntu:
+
+```bash
+git clone --recursive-submodules git@github.com:PedroBinotto/.dotfiles.git
+cd .dotfiles
+./install
+```
+
+---
+
+TODO:
+- [ ] Neovim: Automatic build from source;
+- [ ] Install NodeJS for LSP configuration;
+- [ ] Install and configure NerdFonts;
+- [ ] Solve `/etc/environment` bug;
+- [ ] Solve possible `shellscripts` bug;
--- nvim/.config/nvim/lua/user/utils.lua
+++ nvim/.config/nvim/lua/user/utils.lua
@@ -8,57 +8,57 @@ local oil = require("user.oil")
 local M = {}
 
 local harpoon_get_paths = function(files)
-  local paths = {}
-  local items = files.items
-  local len = files._length
+	local paths = {}
+	local items = files.items
+	local len = files._length
 
-  for i = 1, len do
-    paths[i] = ''
-    local item = items[i]
-    if item ~= nil then
-      paths[i] = item.value
-    end
-  end
+	for i = 1, len do
+		paths[i] = ''
+		local item = items[i]
+		if item ~= nil then
+			paths[i] = item.value
+		end
+	end
 
-  return paths
+	return paths
 end
 
 local function harpoon_make_finder(paths)
-  return telescope_finders.new_table({ results = paths })
+	return telescope_finders.new_table({ results = paths })
 end
 
 function M.oil_cwd()
-  oil.toggle_float(".")
+	oil.toggle_float(".")
 end
 
 function M.oil_find_file()
-  local path = vim.fn.expand("%:h")
-  if vim.fn.findfile(path) then
-    oil.toggle_float(path)
-  else
-    M.oil_cwd()
-  end
+	local path = vim.fn.expand("%:h")
+	if vim.fn.findfile(path) then
+		oil.toggle_float(path)
+	else
+		M.oil_cwd()
+	end
 end
 
 function M.toggle_telescope(harpoon_files)
-  local file_paths = harpoon_get_paths(harpoon_files)
-  telescope_pickers
-      .new({}, {
-        prompt_title = "Harpoon",
-        finder = harpoon_make_finder(file_paths),
-        previewer = telescope_conf.file_previewer({}),
-        sorter = telescope_conf.generic_sorter({}),
-        attach_mappings = function(prompt_buffer_number, map)
-          map("i", "<M-d>", function()
-            local selected_entry = telescope_state.get_selected_entry()
-            local current_picker = telescope_state.get_current_picker(prompt_buffer_number)
-            harpoon:list():remove_at(selected_entry.index)
-            current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list())))
-          end)
-          return true
-        end,
-      })
-      :find()
+	local file_paths = harpoon_get_paths(harpoon_files)
+	telescope_pickers
+			.new({}, {
+				prompt_title = "Harpoon",
+				finder = harpoon_make_finder(file_paths),
+				previewer = telescope_conf.file_previewer({}),
+				sorter = telescope_conf.generic_sorter({}),
+				attach_mappings = function(prompt_buffer_number, map)
+					map("i", "<M-d>", function()
+						local selected_entry = telescope_state.get_selected_entry()
+						local current_picker = telescope_state.get_current_picker(prompt_buffer_number)
+						harpoon:list():remove_at(selected_entry.index)
+						current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list())))
+					end)
+					return true
+				end,
+			})
+			:find()
 end
 
 function M.is_empty(string)
