b55efbbe79b7ba883893c06555dd07c5ea1863c3
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-02-07 15:23:55 -0300

    07-02-2025

--- nvim/.config/nvim/lua/user/comment.lua
+++ nvim/.config/nvim/lua/user/comment.lua
@@ -1,27 +1,27 @@
 local status_comment_ok, comment = pcall(require, "Comment")
 if not status_comment_ok then
-	print("Failed to load module 'Comment'")
-	return
+  print("Failed to load module 'Comment'")
+  return
 end
 
 local status_context_ok, context = pcall(require, "ts_context_commentstring")
 if not status_context_ok then
-	print("Failed to load module 'ts_context_commentstring'")
-	return
+  print("Failed to load module 'ts_context_commentstring'")
+  return
 end
 
 context.setup({
-	enable_autocmd = false,
-	languages = {
-		typescript = "// %s",
-	},
+  enable_autocmd = false,
+  languages = {
+    typescript = "// %s",
+  },
 })
 
 comment.setup({
-	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-	mappings = {
-		basic = false,
-	},
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  mappings = {
+    basic = true,
+  },
 })
 
 return comment
--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -1,9 +1,9 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-	print("Failed to load module 'lspconfig'")
-	return
+  print("Failed to load module 'lspconfig'")
+  return
 end
 
 require("user.lsp.mason")
 require("user.lsp.handlers").setup()
-require("user.lsp.null-ls")
+-- require("user.lsp.null-ls")
