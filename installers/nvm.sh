4bae0ac1fb0ee8c7a272646086dd140322e2e863
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-04-15 17:02:57 -0300

    minor fixes

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 zsh -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash'
-zsh -c 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; zsh -c "nvm install node"'
+zsh -c 'export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm install node'
