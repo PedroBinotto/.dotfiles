2b6ef86bc020ceb860a001486cbae1e2bc3c4839
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-04-15 14:37:06 -0300

    fix

--- .helpers/kitty.sh
+++ .helpers/kitty.sh
@@ -2,7 +2,9 @@
 
 cd ~/projects
 
-git clone https://github.com/kovidgoyal/kitty.git && cd kitty
+git clone https://github.com/kovidgoyal/kitty.git
+sudo mv kitty /opt
+cd /opt/kitty
 ./dev.sh build
 
 # TODO: add .desktop entry
