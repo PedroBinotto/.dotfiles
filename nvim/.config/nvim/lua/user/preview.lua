7b57219aec584ab226773f8d6e80d6db9b2d67e6
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-07-08 18:22:11 -0300

    vimwiki config

--- nvim/.config/nvim/lua/user/preview.lua
+++ nvim/.config/nvim/lua/user/preview.lua
@@ -4,28 +4,29 @@ local mkpreview = require("user.markdown-preview")
 local M = {}
 
 local function preview_markdown()
-	mkpreview.setup()
-	vim.cmd("MarkdownPreviewToggle")
+  mkpreview.setup()
+  vim.cmd("MarkdownPreviewToggle")
 end
 
 local function preview_latex()
-	latex.latex_start_preview()
+  latex.latex_start_preview()
 end
 
 function M.stop_preview() end
 
 function M.preview()
-	local function_map = {
-		markdown = preview_markdown,
-		tex = preview_latex,
-	}
+  local function_map = {
+    markdown = preview_markdown,
+    vimwiki = preview_markdown,
+    tex = preview_latex,
+  }
 
-	local filetype = vim.bo.filetype
-	if function_map[filetype] then
-		function_map[filetype]()
-	else
-		print("No preview method available for filetype '" .. filetype .. "'.")
-	end
+  local filetype = vim.bo.filetype
+  if function_map[filetype] then
+    function_map[filetype]()
+  else
+    print("No preview method available for filetype '" .. filetype .. "'.")
+  end
 end
 
 return M
--- nvim/.config/nvim/lua/user/vimwiki.lua
+++ nvim/.config/nvim/lua/user/vimwiki.lua
@@ -2,7 +2,7 @@ local cmd = [[
 let wiki = {}
 let wiki.path = '~/vimwiki/'
 let wiki.nested_syntaxes = { 'python': 'python', 'java': 'java', 'kotlin': 'kotlin', 'c++': 'cpp', 'c': 'c' }
-let g:vimwiki_list = [wiki]
+let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
 let g:vimwiki_global_ext = 0
 ]]
 
