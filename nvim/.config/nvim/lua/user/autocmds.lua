71786250ccf89524e94754f1a7cb3e30cac02659
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-07-16 21:02:25 -0300

    add vimwiki template logic

--- nvim/.config/nvim/lua/user/autocmds.lua
+++ nvim/.config/nvim/lua/user/autocmds.lua
@@ -1,15 +1,25 @@
 local function configure_format_on_save()
-	local opts = require("user.config.defaults").format_on_save
-	vim.api.nvim_create_augroup("lsp_format_on_save", {})
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		group = "lsp_format_on_save",
-		pattern = opts.pattern,
-		callback = function()
-			require("user.lsp.utils").format({ timeout_ms = opts.timeout, filter = opts.filter })
-		end,
-	})
+  local opts = require("user.config.defaults").format_on_save
+  vim.api.nvim_create_augroup("lsp_format_on_save", {})
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "lsp_format_on_save",
+    pattern = opts.pattern,
+    callback = function()
+      require("user.lsp.utils").format({ timeout_ms = opts.timeout, filter = opts.filter })
+    end,
+  })
+end
+
+local function enable_vimwiki_diary_template()
+  vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*/vimwiki/diary/*.md",
+    callback = function(_)
+      vim.cmd("silent 0r !~/vimwiki/diary_template.py ")
+    end,
+  })
 end
 
 configure_format_on_save()
+enable_vimwiki_diary_template()
 
 -- TODO: autocmd numbertoggle
