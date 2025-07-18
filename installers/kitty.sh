428ff1b1a68385853af7f1b7569aab3e590b3529
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-07-18 19:21:30 -0300

    fixkitty desktop file

--- installers/kitty.sh
+++ installers/kitty.sh
@@ -6,6 +6,7 @@ git clone https://github.com/kovidgoyal/kitty.git
 sudo mv kitty /opt
 cd /opt/kitty
 ./dev.sh build
-sudo cp $SCRIPT_DIR/installers/kitty.desktop /usr/share/applications
+cd ~/.dotfiles
+sudo cp ./installers/kitty.desktop /usr/share/applications
 
 cd ~
