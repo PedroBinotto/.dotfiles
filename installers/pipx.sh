4ce8b14d6bd1e0c211ce49acd314c324d7661bf8
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-07-22 16:51:08 -0300

    fix `pipx` install

--- installers/pipx.sh
+++ installers/pipx.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+cd ~/projects
+
+# FIXME: Solucao provisoria, ver https://github.com/pypa/pipx/issues/1524
+# Quando resolvido, reverter:
+# 1.:
+# ```diff
+# + sudo pipx ensurepath --global
+# ```
+# 2.: Readicionar `pipx` em `../packagefile.json.apt`
+
+git clone git@github.com:zinc75/pipx-1.6.0-backport-ubuntu-2404-lts.git pipx
+cd pipx
+sudo dpkg -i python-pipx_1.6.0-1ubuntu24.04-backport1_all.deb
+sudo apt -f install
 sudo pipx ensurepath --global
+
+cd ~
--- zsh/.keymaps.zsh
+++ zsh/.keymaps.zsh
@@ -2,7 +2,7 @@
 
 bindkey -s "^f" "tmux-sessionizer\n"
 bindkey -s "^[a" "tmux-sessionizer ~/projects/bridge/sgtes-residencias/frontend\n"
-bindkey -s "^[s" "tmux-sessionizer ~/projects/bridge/pec/frontend\n"
+bindkey -s "^[s" "tmux-sessionizer ~/projects/bridge/pec-worktree/pec/frontend\n"
 bindkey -s "^[d" "tmux-sessionizer ~/projects/bridge/pec-assinador/frontend\n"
 bindkey -s "^[j" "tmux-sessionizer ~/shellscripts\n"
 bindkey -s "^[k" "tmux-sessionizer ~/.dotfiles\n"
