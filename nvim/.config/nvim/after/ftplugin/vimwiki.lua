e4c73f9825a087200414df0ca4a0f11c320dba0f
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-09-18 14:45:56 -0300

    remove markdown autocmd

--- nvim/.config/nvim/after/ftplugin/vimwiki.lua
+++ nvim/.config/nvim/after/ftplugin/vimwiki.lua
@@ -1,3 +1,8 @@
 local keymap = vim.keymap.set
 
+keymap('v', '<C-b>', ":lua require('markdowny').bold()<cr>", { buffer = true })
+keymap('v', '<C-i>', ":lua require('markdowny').italic()<cr>", { buffer = true })
+keymap('v', '<C-k>', ":lua require('markdowny').link()<cr>", { buffer = true })
+keymap('v', '<C-e>', ":lua require('markdowny').code()<cr>", { buffer = true })
+
 keymap("n", "<leader>wp", ":VimwikiAll2HTML<CR>", { noremap = true, silent = true, buffer = true })
