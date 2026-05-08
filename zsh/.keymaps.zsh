04a555a952da5fd2aed91ac61959624e84123194
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-05-08 14:23:14 -0300

    fix: zsh keymaps

--- zsh/.keymaps.zsh
+++ zsh/.keymaps.zsh
@@ -1,9 +1,9 @@
 #/bin/zsh
 
+bindkey -v
 bindkey -s "^f" "tmux-sessionizer\n"
 bindkey -s "^[a" "tmux-sessionizer ~/projects/blockful/anticapture\n"
 bindkey -s "^[j" "tmux-sessionizer ~/.dotfiles/shellscripts\n"
 bindkey -s "^[k" "tmux-sessionizer ~/.dotfiles\n"
 bindkey -s "^[u" "tmux-sessionizer ~/Documents/UFSC\n"
 bindkey -s "^[w" "tmux-sessionizer ~/vimwiki\n"
-bindkey -v
